862087217a67d503af01c339ef6d3a3097218a6c
mismail20 <Mohamed.ismailbda@gmail.com>
2021-12-29 02:43:15 +0000

    working expand and delete buttons on fields

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import DeleteIcon from "@material-ui/icons/Delete";
+import ExpandIcon from '@material-ui/icons/ExpandMore';
+import MinimizeIcon from '@material-ui/icons/ExpandLess';
+import DeleteIcon from "@material-ui/icons/Remove";
 import Zoom from "@material-ui/core/Zoom";
 
 
@@ -9,11 +11,14 @@ export default function Field(props) {
 
   const [input, setInput,] = useState({title: "", input1:"", input2:"", input3:"", desc1:"" });
 
+  const [buttonText, setButtonText] = useState(<ExpandIcon/>);
+
   function expand() {
      isExpanded === true ? setExpanded(false): setExpanded(true)
+     isExpanded === true ? setButtonText(<ExpandIcon/>): setButtonText(<MinimizeIcon/>);
   }
 
-  function handleClick(){
+  function del(){
     props.onDelete(props.id);
   }
 
@@ -31,17 +36,15 @@ export default function Field(props) {
   return(
     <div>
       <form>
+        <h3 name="title">{props.title}</h3>
+        {isExpanded && (<input name = "input1" value = {input['input1']} placeholder = {props.input1} onChange = {handleChange} />)}
+        {isExpanded && props.title !== 'Skill' && <input name = "input2" value = {input['input2']} placeholder = {props.input2} onChange = {handleChange} /> }
+        {isExpanded && props.title !== 'Skill' && (<input name = "input3" value = {input['input3']} placeholder = {props.input3} onChange = {handleChange} />)}
+        {isExpanded && props.title !== 'Skill' && (<textarea name = "desc" value={input['desc']} placeholder = {props.desc}  onChange = {handleChange}/> )}
 
-        <div onClick = {expand}>
-            <h3 name="title">{props.title}</h3>
-        </div>
-          {isExpanded && (<input name = "input1" value = {input['input1']} placeholder = {props.input1} onChange = {handleChange} />)}
+        <button className = "expand"  type ="button" onClick = {expand}> {buttonText} </button>
 
-          {isExpanded && props.title !== 'Skill' && <input name = "input2" value = {input['input2']} placeholder = {props.input2} onChange = {handleChange} /> }
-          {isExpanded && props.title !== 'Skill' && (<input name = "input3" value = {input['input3']} placeholder = {props.input3} onChange = {handleChange} />)}
-          {isExpanded && props.title !== 'Skill' && (<textarea name = "desc" value={input['desc']} placeholder = {props.desc}  onChange = {handleChange}/> )}
-
-        <Zoom in= {true}><button type ="button" onClick = {handleClick}> <DeleteIcon/> </button></Zoom>
+        <Zoom in= {true}><button className = "delete" type ="button" onClick = {del}> <DeleteIcon/> </button></Zoom>
       </form>
     </div>
   );
